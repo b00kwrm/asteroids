2911a628aa01669a97b12b3b1593dd5c580d0f7f
dave <dave@harpo.marx>
2018-10-28 06:32:27 -0400

    Added black to do code formatting. Ran black on asteroids.py

--- asteroids.py
+++ asteroids.py
@@ -10,6 +10,7 @@ from pprint import pprint
 dt = datetime.datetime.today()
 today = dt.strftime("%Y-%m-%d")
 
+
 def download_asteroids(start_date, end_date, api_key):
     "Download asteroid data from NASA"
     base_uri = "https://api.nasa.gov/neo/rest/v1/feed?"
@@ -19,36 +20,35 @@ def download_asteroids(start_date, end_date, api_key):
     asteroid_data = r.json()
     return asteroid_data
 
+
 def filter_asteroid_data(asteroid_data, todays_date):
-    "Filter the asteroid data to pull out the name, size speed and danger"
+    "Filter the asteroid data to pull out the name, size, speed, and danger"
     asteroids = []
-    for asteroid in asteroid_data['near_earth_objects'][todays_date]:
+    for asteroid in asteroid_data["near_earth_objects"][todays_date]:
         filtered_data = {
-            'name': asteroid['name'],
-            'size': asteroid['estimated_diameter']['meters']['estimated_diameter_min'],
-            'danger': asteroid['is_potentially_hazardous_asteroid']
+            "name": asteroid["name"],
+            "size": asteroid["estimated_diameter"]["meters"]["estimated_diameter_min"],
+            "danger": asteroid["is_potentially_hazardous_asteroid"],
         }
-        for close_approach_data in asteroid['close_approach_data']:
-            filtered_data["speed"] = close_approach_data['relative_velocity']['miles_per_hour']
+        for close_approach_data in asteroid["close_approach_data"]:
+            filtered_data["speed"] = close_approach_data["relative_velocity"][
+                "miles_per_hour"
+            ]
         asteroids.append(filtered_data)
     return asteroids
+
 
 def create_csv(data, output_file):
     "Create a csv from a list of dicts"
     field_names = list(data[0].keys())
-    with open(output_file, 'w') as csvfile:
+    with open(output_file, "w") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=field_names)
         writer.writeheader()
         writer.writerows(data)
 
-    
+
 if __name__ == "__main__":
     asteroid_data = download_asteroids(today, today, "DEMO_KEY")
     asteroids = filter_asteroid_data(asteroid_data, today)
     pprint(asteroids)
     create_csv(asteroids, f"asteroids_{today}.csv")
-    
-
-
-    
-    
